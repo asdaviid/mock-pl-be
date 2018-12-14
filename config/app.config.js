2507f57dab689777c4332ae64a464482a39292d1
David Alamu <aestrex@gmail.com>
2018-12-14 22:00:06 +0100

    remove unused config vars

--- config/app.config.js
+++ config/app.config.js
@@ -1,10 +1,5 @@
 module.exports = {
   jwtSecret: 'jwts3cr3t',
   jwtSession: { session: false },
-  mongoUri: process.env.NODE_ENV === 'development' ? 'mongodb://localhost:27017/mock-pl' : process.env.MONGO_URI,
-  database: process.env.NODE_ENV === 'development' ? 'mock-pl-be' : process.env.DB_NAME,
-  username: process.env.NODE_ENV === 'development' ? 'mock-pl-be' : process.env.DB_USERNAME,
-  password: process.env.NODE_ENV === 'development' ? 'mock-pl-be' : process.env.DB_PASSWORD,
-  host: process.env.NODE_ENV === 'development' ? '127.0.0.1' : process.env.DB_HOST,
-  dialect: 'mysql'
+  mongoUri: process.env.NODE_ENV === 'development' ? 'mongodb://localhost:27017/mock-pl' : process.env.MONGO_URI
 }
