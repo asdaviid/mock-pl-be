5f5e706913615791df25e1e7e4b5341a0686a94d
David Alamu <aestrex@gmail.com>
2018-12-15 15:35:14 +0100

    set default mongo uri

--- config/app.config.js
+++ config/app.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   jwtSecret: 'jwts3cr3t',
   jwtSession: { session: false },
-  mongoUri: process.env.NODE_ENV === 'development' ? 'mongodb://localhost:27017/mock-pl' : process.env.MONGO_URI
+  mongoUri: process.env.MONGO_URI || 'mongodb://localhost:27017/mock-pl'
 }
