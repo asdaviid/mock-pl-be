4c91cbe68546a3d14b20fd29eea2167796f80310
David Alamu <aestrex@gmail.com>
2018-12-15 22:49:15 +0100

    combine search results

--- controllers/search.controller.js
+++ controllers/search.controller.js
@@ -2,70 +2,40 @@ const Stadium = require('../models/stadium.model');
 const Fixture = require('../models/fixture.model');
 const Team = require('../models/team.model');
 
-const search = (req, res) => {
-  const { collection, query } = req.query;
+const search = async (req, res) => {
+  const { query } = req.query;
 
-  switch (collection) {
-    case 'fixture':
-      Fixture.find({ 
-        "$or": [
-          { 'competition': { '$regex': query, '$options': 'i' } }
-        ]
-      }, '_id competition kickoff venue')
-      .populate('home_team', '_id name website founded country')
-      .populate('away_team', '_id name website founded country')
-      .exec((err, fixtures) => {
-        if (err) {
-          throw err;
-        }
+  const fixtures = await Fixture.find({ 
+    "$or": [
+      { 'competition': { '$regex': query, '$options': 'i' } }
+    ]
+  }, '_id competition kickoff venue')
+  .populate('home_team', '_id name website founded country')
+  .populate('away_team', '_id name website founded country').exec();
 
-        return res.status(200).json({
-          fixtures
-        });
-      });
-      break;
-    case 'team':
-      Team.find({ 
-        "$or": [
-          { 'name': { '$regex': query, '$options': 'i' } },
-          { 'website': { '$regex': query, '$options': 'i' } },
-          { 'founded': { '$regex': query, '$options': 'i' } },
-          { 'country': { '$regex': query, '$options': 'i' } }
-        ]
-      }, '_id name website founded country')
-      .populate('home_stadium', '_id name city capacity')
-      .exec((err, teams) => {
-        if (err) {
-          throw err;
-        }
+  const teams = await Team.find({ 
+    "$or": [
+      { 'name': { '$regex': query, '$options': 'i' } },
+      { 'website': { '$regex': query, '$options': 'i' } },
+      { 'founded': { '$regex': query, '$options': 'i' } },
+      { 'country': { '$regex': query, '$options': 'i' } }
+    ]
+  }, '_id name website founded country')
+  .populate('home_stadium', '_id name city capacity').exec();
 
-        return res.status(200).json({
-          teams
-        });
-      });
-      break;
-    case 'stadium':
-      Stadium.find({
-        "$or": [
-          { 'name': { '$regex': query, '$options': 'i' } },
-          { 'city': { '$regex': query, '$options': 'i' } },
-          { 'capacity': { '$regex': query, '$options': 'i' } }
-        ]
-      }, '_id name city capacity').exec((err, stadia) => {
-        if (err) {
-          throw err;
-        }
+  const stadia = await Stadium.find({
+    "$or": [
+      { 'name': { '$regex': query, '$options': 'i' } },
+      { 'city': { '$regex': query, '$options': 'i' } },
+      { 'capacity': { '$regex': query, '$options': 'i' } }
+    ]
+  }, '_id name city capacity').exec();
 
-        return res.status(200).json({
-          stadia
-        });
-      });
-      break;
-    default:
-      return res.status(400).json({
-        message: 'Invalid collection queried'
-      });
-  }
+  return res.status(200).json({
+    teams,
+    fixtures,
+    stadia
+  });
 }
 
 module.exports = {
